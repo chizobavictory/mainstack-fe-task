5233393f1feb31a61d8cb6783b003fd60bc45435
Chizoba Victory <chizoba.victory1@gmail.com>
2023-10-30 09:56:45 +0100

    added modal compoonents

--- src/components/Transactions.tsx
+++ src/components/Transactions.tsx
@@ -3,6 +3,7 @@ import expand from "../assets/expand_more.svg";
 import download from "../assets/download.svg";
 import TransactionsList from "./TransactionsList";
 import FilterModal from "./Modal";
+import useTransactionData from "../hooks/transactions";
 
 const Transactions = () => {
   const [isFilterModalOpen, setFilterModalOpen] = useState(false);
@@ -14,12 +15,15 @@ const Transactions = () => {
   const closeFilterModal = () => {
     setFilterModalOpen(false);
   };
+  const transactionData = useTransactionData();
+  const totalTransactions = transactionData ? transactionData.length : 0;
+
 
   return (
     <div className="flex flex-col pt-20">
       <div className="flex justify-between">
         <div className="text-neutral-900 flex flex-col gap-1">
-          <p className="font-[degularbold] text-2xl">24 Transactions</p>
+          <p className="font-[degularbold] text-2xl">{totalTransactions} Transactions</p>
           <p className="font-[degularmedium] text-gray-600">Your transactions for the last 7 days</p>
         </div>
         <div className="flex gap-2">
